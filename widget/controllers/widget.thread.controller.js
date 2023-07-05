fb810f6e3affab5a394563050986bc3377a31a7b
Hiba-Almade <hibaalmade998@gmail.com>
2023-07-05 16:53:33 +0300

    fix button display

--- widget/controllers/widget.thread.controller.js
+++ widget/controllers/widget.thread.controller.js
@@ -87,8 +87,9 @@
             Thread.setAppTheme = function () {
                 buildfire.appearance.getAppTheme((err, obj) => {
                     let elements = document.getElementsByTagName('svg');
-                    document.getElementById('addBtn').style.setProperty("background-color", obj.colors.icons, "important");
+                    document.getElementsByClassName("add-new-comment-btn")[0].style.setProperty("background-color", obj.colors.icons, "important");
                     elements[3].style.setProperty("fill", obj.colors.titleBarTextAndIcons, "important");
+                    document.getElementById("add-comment-svg").style.setProperty("fill", 'white', "important");
                 });
             }
 
