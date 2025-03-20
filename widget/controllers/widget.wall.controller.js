ac0271ee12d03d5ab34ca33c743a5dfd92a675eb
alaa smadi <alaasmadi1010@gmail.com>
2025-03-21 00:45:53 +0300

    revert auth manager

--- widget/controllers/widget.wall.controller.js
+++ widget/controllers/widget.wall.controller.js
@@ -2,7 +2,7 @@
 
 (function (angular) {
     angular.module('socialPluginWidget')
-      .controller('WidgetWallCtrl', ['$scope', 'SocialDataStore', 'Buildfire', '$rootScope', 'Location', 'EVENTS', 'GROUP_STATUS', 'MORE_MENU_POPUP', 'FILE_UPLOAD', 'SocialItems', '$q', '$anchorScroll', '$location', '$timeout', 'Util', 'SubscribedUsersData', 'AuthManager', function ($scope, SocialDataStore, Buildfire, $rootScope, Location, EVENTS, GROUP_STATUS, MORE_MENU_POPUP, FILE_UPLOAD, SocialItems, $q, $anchorScroll, $location, $timeout, util, SubscribedUsersData, AuthManager) {
+      .controller('WidgetWallCtrl', ['$scope', 'SocialDataStore', 'Buildfire', '$rootScope', 'Location', 'EVENTS', 'GROUP_STATUS', 'MORE_MENU_POPUP', 'FILE_UPLOAD', 'SocialItems', '$q', '$anchorScroll', '$location', '$timeout', 'Util', 'SubscribedUsersData', function ($scope, SocialDataStore, Buildfire, $rootScope, Location, EVENTS, GROUP_STATUS, MORE_MENU_POPUP, FILE_UPLOAD, SocialItems, $q, $anchorScroll, $location, $timeout, util, SubscribedUsersData) {
           var WidgetWall = this;
 
           WidgetWall.userDetails = {};
@@ -580,11 +580,7 @@
                                       WidgetWall.checkForPrivateChat();
                                       WidgetWall.checkForDeeplinks();
                                   } else {
-                                    AuthManager.enforceLogin((user) => {
-                                      WidgetWall.checkFollowingStatus(user);
-                                      WidgetWall.checkForPrivateChat();
-                                      WidgetWall.checkForDeeplinks();
-                                    })
+                                    WidgetWall.groupFollowingStatus = false;
                                   }
                               });
                           });
@@ -1619,7 +1615,6 @@
                     WidgetWall.SocialItems.context.instanceId : WidgetWall.SocialItems.wid
               }, () => {});
               WidgetWall.privateChatSecurity();
-              AuthManager.enforceLogin(WidgetWall.init);
               $scope.$digest();
           });
 
