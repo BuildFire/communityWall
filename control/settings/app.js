93fbed64a2a289722382862baa72101193dfbdfd
alialaqrabawi3 <aalaqrabawi@madaincorp.com>
2025-09-15 01:18:39 +0300

    use standard tag input for the side thread user tags

--- control/settings/app.js
+++ control/settings/app.js
@@ -3,11 +3,9 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('socialPluginSettings', [
     'ngRoute',
-    'ngTagsInput'
 ]);
-app.config(['$locationProvider', '$routeProvider', 'tagsInputConfigProvider', function ($locationProvider, $routeProvider, tagsInputConfigProvider) {
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
     //config for tag-input plugin
-    tagsInputConfigProvider.setActiveInterpolation('tagsInput', { minTags: true });
 
     $routeProvider.when('/', {
         templateUrl: 'views/mainSettings.html',
